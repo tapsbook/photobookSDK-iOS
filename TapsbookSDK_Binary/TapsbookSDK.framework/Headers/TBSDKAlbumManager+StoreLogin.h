1f6aae1d047b7693bd7fec8b467b3ce97a1919ed
sherwood <sherwoodyao@gmail.com>
2016-12-03 23:31:50 -0500

    add 3rd party user reg API to SDK

--- TapsbookSDK_Binary/TapsbookSDK.framework/Headers/TBSDKAlbumManager+StoreLogin.h
+++ TapsbookSDK_Binary/TapsbookSDK.framework/Headers/TBSDKAlbumManager+StoreLogin.h
@@ -17,10 +17,22 @@ typedef NS_ENUM(NSInteger, TBSDKLoginRegisterResult) {
 
 @interface TBSDKAlbumManager (StoreLogin)
 
+///---------------------------------------
+/// @name Link your app user to SDK user
+///---------------------------------------
+
 - (void)presentStoreLoginViewControllerOnViewController:(UIViewController *)viewController completionBlock:(void (^)(TBSDKLoginRegisterResult result, TBSDKUser *sdkUser, NSError *error))completionBlock;
 
 - (void)logoutCurrentUser;
 
 - (TBSDKUser *)currentSDKUser;
+
+/**
+ * SDK backend has a user registry that is used to associate user orders with the user. You must perform this signin process if you wish to retrieve order status later.
+ *  @param userid The userid in your app, it must be unique.
+ *  @param appName The appName of your app, it must be unique, use your app bundle id will be a good start.
+ *  @param completionBlock The callback after signin is complete. You can use this to find out whether the signin was successful.
+ */
+- (void) signinSDKUser:(NSString *) userid fromApp:(NSString*)appName completionBlock:(void (^)(BOOL success, TBSDKUser *sdkUser, NSError *error))completionBlock;
 
 @end
--- TapsbookSDK_Binary/TapsbookSDK.framework/Versions/A/Headers/TBSDKAlbumManager+StoreOrderList.h
+++ TapsbookSDK_Binary/TapsbookSDK.framework/Versions/A/Headers/TBSDKAlbumManager+StoreOrderList.h
@@ -10,6 +10,16 @@
 
 @interface TBSDKAlbumManager (StoreOrderList)
 
+///---------------------------------------
+/// @name Present built-in UI for showing orders for a signed-in SDK user
+///---------------------------------------
+
+/**
+ * Present a view to show all orders submitted by a given user. User can view the order status and the detail for each order.
+ * 
+ *  @param viewController The VC you want to present the order list.
+ */
+
 - (void)presentOrderListViewControllerOnViewController:(UIViewController *)viewController;
 
 - (UIViewController *)orderListViewController;
